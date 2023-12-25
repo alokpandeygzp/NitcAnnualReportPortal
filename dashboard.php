1af6f97a13ce2090de84436649b5cdd7e77d2254
Junaid Ansari <junaidansari21975@gmail.com>
2023-12-25 16:35:08 +0530

    dashboard completed.

--- dashboard.php
+++ dashboard.php
@@ -14,8 +14,10 @@ if (empty($_SESSION['access_token'])) {
 
 <head>
     <title>Dashboard</title>
-    <link rel="stylesheet" href="">
-    <link href="./styles/styles.css" type="text/css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./styles/dashboard.css" type="text/css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -29,7 +31,7 @@ if (empty($_SESSION['access_token'])) {
 
 
 
-        <!-- <a href="./doc/pdf.php"><button class="">Generate Report</button></a> -->
+
         <?php
         echo '<div class="user_strip">
                 <div class="user">
@@ -100,13 +102,90 @@ if (empty($_SESSION['access_token'])) {
                 <div class="content_identifier">
                     Generate Report
                 </div>
-                <div>
+                <div class="report_generator">
+                    <div class="section_container">
+                        <p>Choose Period:</p>
+                        <div class="time_period_selector">
+                            <div class="start_date">
+                                <label for="startDate">Start: </label>
+                                <input type="date" id="startDate" name="startDate" class="input_field" />
+                            </div>
+                            <div class="end_date">
+                                <label for="endDate">End: </label>
+                                <input type="date" id="endDate" name="endDate" class="input_field" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section_container">
+                        <p>Choose Sections:</p>
+                        <div class="section_selector">
+                            <div>
+                                <input type="checkbox" id="all" name="all" value="all" onchange="selectAllOptions()" />
+                                <label for="all">All</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="communityServices" name="communityServices"
+                                    value="communityServices" />
+                                <label for="communityServices">Community Services</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="otherServices" name="otherServices" value="otherServices" />
+                                <label for="otherServices">Other Services</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="conferencesConducted" name="conferencesConducted"
+                                    value="conferencesConducted" />
+                                <label for="conferencesConducted">Conferences Conducted</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="expertLectures" name="expertLectures"
+                                    value="expertLectures" />
+                                <label for="expertLectures">Expert Lectures</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="facultyHigherQualification" name="facultyHigherQualification"
+                                    value="facultyHigherQualification" />
+                                <label for="facultyHigherQualification">Faculty Higher Qualification</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="consultancyAndTesting" name="consultancyAndTesting"
+                                    value="consultancyAndTesting" />
+                                <label for="consultancyAndTesting">Consultancy And Testing</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="patentAquiredAndFiled" name="patentAquiredAndFiled"
+                                    value="patentAquiredAndFiled" />
+                                <label for="patentAquiredAndFiled">Patent Aquired And Filed</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="studentAchievements" name="studentAchievements"
+                                    value="studentAchievements" />
+                                <label for="studentAchievements">Student Achievements</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <a href="./doc/pdf.php"><button class="generate_btn">Generate Report</button></a>
 
                 </div>
             </div>
         </div>
 
     </div>
+    <script>
+        function selectAllOptions() {
+            // Get the "All" checkbox
+            var allCheckbox = document.getElementById("all");
+
+            // Get all the checkboxes excluding the "All" checkbox
+            var checkboxes = document.querySelectorAll('.section_selector input[type="checkbox"]:not(#all)');
+
+            // Set the state of other checkboxes based on the "All" checkbox
+            checkboxes.forEach(function (checkbox) {
+                checkbox.checked = allCheckbox.checked;
+            });
+        }
+    </script>
 </body>
 
 </html>
