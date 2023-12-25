aee91e2d83fb4e1f1d0e5c9f5e096a0696baf8c9
arham-siddiki <arhamsiddiki9643@gmail.com>
2023-12-25 23:49:18 +0530

    Update imsdemo.sql

--- database/imsdemo.sql
+++ database/imsdemo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2023 at 12:09 PM
+-- Generation Time: Dec 25, 2023 at 03:13 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,18 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `community_services` (
   `staff` varchar(80) NOT NULL,
-  `title` varchar(2000) NOT NULL
+  `title` varchar(2000) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `community_services`
 --
 
-INSERT INTO `community_services` (`staff`, `title`) VALUES
-('Dr. M. Surya Prakash', 'Worked as a Reviewer for National Conference on “Advances in  Computing, Communication, Signals, Energy and Technology” held at  College of Engineering Vadakara, from 26th May, 2021 to 27th May,  2021.  • Worked as a Reviewer for “The Journal of Institute of the Engineers”,  2021-22.'),
-('Dr. V. Sakthivel', 'Appointed as Examiner for the PhD Thesis Evaluation of VIT Vellore,  Tamilnadu in October 2021.  • 2. Appointed as Observer for the Joint CSIR-UGC NET Exam,  June 2021 Phase II conducted by NTA, Ministry of Education, India. • 3. Appointed as Observer for the NEET UG Exam, August 2021  conducted by NTA, Ministry of Education, India. • 4. Secretary of Faculty Association of NIT Calicut (From June 2021 to till  date).'),
-('B. Bhuvan', '1. Reviewer, IEEE Sensors Journal • 2. Reviewer, Swadeshi Microprocessor challenge 2021 • 3. Member, External review panel for Embedded Systems &  VLSI, NPOL'),
-('DR. SD Madhu Kumar', 'final sem project mca');
+INSERT INTO `community_services` (`staff`, `title`, `entity`) VALUES
+('Dr. M. Surya Prakash', 'Worked as a Reviewer for National Conference on “Advances in  Computing, Communication, Signals, Energy and Technology” held at  College of Engineering Vadakara, from 26th May, 2021 to 27th May,  2021.  • Worked as a Reviewer for “The Journal of Institute of the Engineers”,  2021-22.', ''),
+('Dr. V. Sakthivel', 'Appointed as Examiner for the PhD Thesis Evaluation of VIT Vellore,  Tamilnadu in October 2021.  • 2. Appointed as Observer for the Joint CSIR-UGC NET Exam,  June 2021 Phase II conducted by NTA, Ministry of Education, India. • 3. Appointed as Observer for the NEET UG Exam, August 2021  conducted by NTA, Ministry of Education, India. • 4. Secretary of Faculty Association of NIT Calicut (From June 2021 to till  date).', ''),
+('B. Bhuvan', '1. Reviewer, IEEE Sensors Journal • 2. Reviewer, Swadeshi Microprocessor challenge 2021 • 3. Member, External review panel for Embedded Systems &  VLSI, NPOL', ''),
+('DR. SD Madhu Kumar', 'final sem project mca', ''),
+('cse1', 's1', 'CSED'),
+('cse2', 's2', 'CSED');
 
 -- --------------------------------------------------------
 
@@ -51,17 +54,19 @@ INSERT INTO `community_services` (`staff`, `title`) VALUES
 CREATE TABLE `conferences` (
   `title` varchar(200) NOT NULL,
   `name` varchar(80) NOT NULL,
-  `duration` varchar(80) NOT NULL
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `conferences`
 --
 
-INSERT INTO `conferences` (`title`, `name`, `duration`) VALUES
-('3D Graphics, Mr. Abhishek  Rhisheekesan, Founder and  CEO at aiRender Technology Pvt Ltd.', 'G Abhilash (IEEE SP  Society SB Chapter)', 'One Hour,  21 March 2022'),
-('Self Sponsored Five-Day  Online Short Term Training  Program (STTP) on “VLSI  Architectures for Digital  Signal Processing Systems”  under Diamond Jubilee  Celebrations of NITC', 'Dr. Ashutosh Mishra, Dr.  M. Surya Prakash', '5 days (14th June, 2021 - 18th June, 2021)'),
-('Student Development  Training Programme on “AIIoT for Real Time  Applications”', 'Raghu C V and  Karthikeyan V (EED)', '5 Days (22/07/2021- 26/07/2021)');
+INSERT INTO `conferences` (`title`, `name`, `start`, `end`, `entity`) VALUES
+('3D Graphics, Mr. Abhishek  Rhisheekesan, Founder and  CEO at aiRender Technology Pvt Ltd.', 'G Abhilash (IEEE SP  Society SB Chapter)', '0000-00-00', NULL, ''),
+('Self Sponsored Five-Day  Online Short Term Training  Program (STTP) on “VLSI  Architectures for Digital  Signal Processing Systems”  under Diamond Jubilee  Celebrations of NITC', 'Dr. Ashutosh Mishra, Dr.  M. Surya Prakash', '0000-00-00', NULL, ''),
+('Student Development  Training Programme on “AIIoT for Real Time  Applications”', 'Raghu C V and  Karthikeyan V (EED)', '0000-00-00', NULL, '');
 
 -- --------------------------------------------------------
 
@@ -73,15 +78,16 @@ CREATE TABLE `consultancy` (
   `nature` varchar(80) NOT NULL,
   `organization` varchar(80) NOT NULL,
   `revenue` int(10) NOT NULL,
-  `status` varchar(80) NOT NULL
+  `status` varchar(80) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `consultancy`
 --
 
-INSERT INTO `consultancy` (`nature`, `organization`, `revenue`, `status`) VALUES
-('Consultancy', 'PhyTunes Inc, USA', 0, 'Ongoing');
+INSERT INTO `consultancy` (`nature`, `organization`, `revenue`, `status`, `entity`) VALUES
+('Consultancy', 'PhyTunes Inc, USA', 0, 'Ongoing', '');
 
 -- --------------------------------------------------------
 
@@ -92,20 +98,22 @@ INSERT INTO `consultancy` (`nature`, `organization`, `revenue`, `status`) VALUES
 CREATE TABLE `expert_lectures` (
   `staff` varchar(80) NOT NULL,
   `title` varchar(80) NOT NULL,
-  `duration` varchar(80) NOT NULL,
-  `organization` varchar(80) NOT NULL
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
+  `organization` varchar(80) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `expert_lectures`
 --
 
-INSERT INTO `expert_lectures` (`staff`, `title`, `duration`, `organization`) VALUES
-('Dr. M. Surya  Prakash', 'International  Conference on  Advances in Signal  Processing and  Communications', '9th July, 2021 - 10th July, 2021', 'Narasaraopet Engg.  College, A. P.'),
-('Sudeep P V', 'KTU sponsored  Faculty Development  Program (FDP) on  \"Deep Learning for  Signal', 'April 2021', 'Sahrudaya College of  Engineering and  Technology, Kerala'),
-('Sudeep P V', 'KTU sponsored  Faculty Development  Program (FDP) on \"Recent developments  in Ma', 'June 2021', 'Toc H Institute of  Science and  Technology, Kerala'),
-('Sudhish N  George', 'Recent Trends in  Moving Object  Detection', 'Jan 2022', 'Navodaya Institute of  Technology, Raichur'),
-('Sudhish N  George', 'Recent Trends in  Moving Object Detection', 'Feb 2022', 'K. S. School of  Engineering and  Management,  Bangalore');
+INSERT INTO `expert_lectures` (`staff`, `title`, `start`, `end`, `organization`, `entity`) VALUES
+('Dr. M. Surya  Prakash', 'International  Conference on  Advances in Signal  Processing and  Communications', '0000-00-00', NULL, 'Narasaraopet Engg.  College, A. P.', ''),
+('Sudeep P V', 'KTU sponsored  Faculty Development  Program (FDP) on  \"Deep Learning for  Signal', '0000-00-00', NULL, 'Sahrudaya College of  Engineering and  Technology, Kerala', ''),
+('Sudeep P V', 'KTU sponsored  Faculty Development  Program (FDP) on \"Recent developments  in Ma', '0000-00-00', NULL, 'Toc H Institute of  Science and  Technology, Kerala', ''),
+('Sudhish N  George', 'Recent Trends in  Moving Object  Detection', '0000-00-00', NULL, 'Navodaya Institute of  Technology, Raichur', ''),
+('Sudhish N  George', 'Recent Trends in  Moving Object Detection', '0000-00-00', NULL, 'K. S. School of  Engineering and  Management,  Bangalore', '');
 
 -- --------------------------------------------------------
 
@@ -116,15 +124,16 @@ INSERT INTO `expert_lectures` (`staff`, `title`, `duration`, `organization`) VAL
 CREATE TABLE `faculty_qualification` (
   `name` varchar(80) NOT NULL,
   `qualification` varchar(200) NOT NULL,
-  `institute` varchar(80) NOT NULL
+  `institute` varchar(80) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `faculty_qualification`
 --
 
-INSERT INTO `faculty_qualification` (`name`, `qualification`, `institute`) VALUES
-('Jaikumar M G', 'PhD', 'IIT Madras');
+INSERT INTO `faculty_qualification` (`name`, `qualification`, `institute`, `entity`) VALUES
+('Jaikumar M G', 'PhD', 'IIT Madras', '');
 
 -- --------------------------------------------------------
 
@@ -134,18 +143,19 @@ INSERT INTO `faculty_qualification` (`name`, `qualification`, `institute`) VALUE
 
 CREATE TABLE `other_services` (
   `staff` varchar(80) NOT NULL,
-  `title` varchar(2000) NOT NULL
+  `title` varchar(2000) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `other_services`
 --
 
-INSERT INTO `other_services` (`staff`, `title`) VALUES
-('Dr. M. Surya Prakash', '(1st April, 2021 to 6th August, 2021) • Time-table In-Charge for ECED (jointly with Dr. Bindiya T. S.). • Faculty Incharge for Electronic Circuits Lab of ECED. • Faculty Advisor for the 2019-23 batch of ECE students (Jointly with  Dr. Praveen Sankaran, Dr. Suja K. J. and Dr. Gopikrishna S.). • Committee Member for Disposal/Write-Off of unserviceable items of  ECED. • Committee Member for preparation of Self-Assessment Report  (SAR) for M. Tech Telecommunications specialization. • Member of the General Arrangements Committee for National Board  of Accreditation Work for Telecommunications Specialization. (7th  August, 2021 to 31st March, 2022) • Time-table In-Charge for ECED (jointly with Dr. Bindiya T. S.). • Faculty Incharge for Electronic Circuits Lab of ECED. • Faculty Advisor for the 2019-23 batch of ECE students (Jointly with  Dr. Praveen Sankaran, Dr. Suja K. J. and Dr. Gopikrishna S.).'),
-('Dr. Lalit Kumar', '• Faculty Advisor for the 2020-24 ECE_Batch-20 • Committee member of CCAR • Exam Vigilance Committee • Coordination of Interview & Document Verification'),
-('Dr. V. Sakthivel', 'Official for the online admission process of JoSSA/CSAB at NIT  Calicut for 2021-22 (UG Admissions) • SPCOM Lab Faculty-in-charge • Member in ECED Exam Vigilance committee • Convener, NITC Sports Council • Coordinator of B. Tech Major Project Evaluation Committee  (Communication/SP) • DC member in EED and CSED. • Member in the committee of coordination of online interview and  document verification (ECED).'),
-('Dr Ameer P.M.', '• Program coordinator - MTech Telecommunication • Lab In charge - Communication Lab');
+INSERT INTO `other_services` (`staff`, `title`, `entity`) VALUES
+('Dr. M. Surya Prakash', '(1st April, 2021 to 6th August, 2021) • Time-table In-Charge for ECED (jointly with Dr. Bindiya T. S.). • Faculty Incharge for Electronic Circuits Lab of ECED. • Faculty Advisor for the 2019-23 batch of ECE students (Jointly with  Dr. Praveen Sankaran, Dr. Suja K. J. and Dr. Gopikrishna S.). • Committee Member for Disposal/Write-Off of unserviceable items of  ECED. • Committee Member for preparation of Self-Assessment Report  (SAR) for M. Tech Telecommunications specialization. • Member of the General Arrangements Committee for National Board  of Accreditation Work for Telecommunications Specialization. (7th  August, 2021 to 31st March, 2022) • Time-table In-Charge for ECED (jointly with Dr. Bindiya T. S.). • Faculty Incharge for Electronic Circuits Lab of ECED. • Faculty Advisor for the 2019-23 batch of ECE students (Jointly with  Dr. Praveen Sankaran, Dr. Suja K. J. and Dr. Gopikrishna S.).', ''),
+('Dr. Lalit Kumar', '• Faculty Advisor for the 2020-24 ECE_Batch-20 • Committee member of CCAR • Exam Vigilance Committee • Coordination of Interview & Document Verification', ''),
+('Dr. V. Sakthivel', 'Official for the online admission process of JoSSA/CSAB at NIT  Calicut for 2021-22 (UG Admissions) • SPCOM Lab Faculty-in-charge • Member in ECED Exam Vigilance committee • Convener, NITC Sports Council • Coordinator of B. Tech Major Project Evaluation Committee  (Communication/SP) • DC member in EED and CSED. • Member in the committee of coordination of online interview and  document verification (ECED).', ''),
+('Dr Ameer P.M.', '• Program coordinator - MTech Telecommunication • Lab In charge - Communication Lab', '');
 
 -- --------------------------------------------------------
 
@@ -156,18 +166,19 @@ INSERT INTO `other_services` (`staff`, `title`) VALUES
 CREATE TABLE `patents` (
   `staff` varchar(80) NOT NULL,
   `title` varchar(800) NOT NULL,
-  `year` varchar(80) NOT NULL
+  `year` varchar(80) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `patents`
 --
 
-INSERT INTO `patents` (`staff`, `title`, `year`) VALUES
-('Vinay Joseph (coinventor)', 'US-2022141789-A1, Delivery time windows  for low latency communications', '2022 (filed)'),
-('Vinay Joseph (co-inventor)', 'US-2022085932-A1, Sounding reference  signals and channel state information  reference signals enhancements for  coordinated multipoint communications', '2021 (filed)'),
-('Vinay Joseph (co-inventor)', 'US-2022046565-A1, Reference timing  delivery to user equipment with propagation  delay compensation', '2021 (filed)'),
-('Vinay Joseph (co-inventor)', 'US-2021367718-A1, Autonomous reference  signal transmission configuration', '2021 (filed)');
+INSERT INTO `patents` (`staff`, `title`, `year`, `entity`) VALUES
+('Vinay Joseph (coinventor)', 'US-2022141789-A1, Delivery time windows  for low latency communications', '2022 (filed)', ''),
+('Vinay Joseph (co-inventor)', 'US-2022085932-A1, Sounding reference  signals and channel state information  reference signals enhancements for  coordinated multipoint communications', '2021 (filed)', ''),
+('Vinay Joseph (co-inventor)', 'US-2022046565-A1, Reference timing  delivery to user equipment with propagation  delay compensation', '2021 (filed)', ''),
+('Vinay Joseph (co-inventor)', 'US-2021367718-A1, Autonomous reference  signal transmission configuration', '2021 (filed)', '');
 
 -- --------------------------------------------------------
 
@@ -177,19 +188,20 @@ INSERT INTO `patents` (`staff`, `title`, `year`) VALUES
 
 CREATE TABLE `student_achievements` (
   `name` varchar(80) NOT NULL,
-  `achievement` varchar(800) NOT NULL
+  `achievement` varchar(800) NOT NULL,
+  `entity` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student_achievements`
 --
 
-INSERT INTO `student_achievements` (`name`, `achievement`) VALUES
-('Anjali Sara Markose', 'Winner of Synopsys Inno Champ 2022 (Female Category)'),
-('Sakhineti Praveena', 'a student of the B19 batch of ECE received the prestigious  Micron’s URAM Scholarship with a scholarship amount of  Rs 1,00,000.'),
-('Diyea Robin', 'a student of B 18 Batch won the Carolyn Leighton  Scholarship 2021 which includes a one-year WITI  membership and an opportunity to attend the 2021 WITI  Global Summit. She also bagged the second position in the  elocution competition conducted in association with the  Azadi Ka Amruth Mahotsav.'),
-('Polamuri Ushaswini', 'won the first prize and runner up respectively in InnoChamp 22, the annual innovation  challenge conducted by Synopsys Inc'),
-('Veena Narayanan', 'Research Scholar (P180043EC) received Special Mention  Award for her paper presented at the IEEE International  Conference on Advanced Communication Technologies and  Signal Processing 2021 (ACTS 2021) NIT Rourkela');
+INSERT INTO `student_achievements` (`name`, `achievement`, `entity`) VALUES
+('Anjali Sara Markose', 'Winner of Synopsys Inno Champ 2022 (Female Category)', ''),
+('Sakhineti Praveena', 'a student of the B19 batch of ECE received the prestigious  Micron’s URAM Scholarship with a scholarship amount of  Rs 1,00,000.', ''),
+('Diyea Robin', 'a student of B 18 Batch won the Carolyn Leighton  Scholarship 2021 which includes a one-year WITI  membership and an opportunity to attend the 2021 WITI  Global Summit. She also bagged the second position in the  elocution competition conducted in association with the  Azadi Ka Amruth Mahotsav.', ''),
+('Polamuri Ushaswini', 'won the first prize and runner up respectively in InnoChamp 22, the annual innovation  challenge conducted by Synopsys Inc', ''),
+('Veena Narayanan', 'Research Scholar (P180043EC) received Special Mention  Award for her paper presented at the IEEE International  Conference on Advanced Communication Technologies and  Signal Processing 2021 (ACTS 2021) NIT Rourkela', '');
 
 --
 -- Indexes for dumped tables
